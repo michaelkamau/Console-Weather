d7db4a20df2364a2c4f80b0ac29f9915184cad0c
Michael Kamau <tokamau@gmail.com>
2016-10-11 11:33:03 +0300

    Add initial definition for degree to direction conversion

---
+++ console_weather/utilities.py
@@ -0,0 +1,11 @@
+def degrees_to_direction(angle):
+    """
+    This function converts an angle (in degrees) to a compass direction
+
+    REFERENCE : http://climate.umn.edu/snow_fence/components/winddirectionanddegreeswithouttable3.htm
+    :param angle:
+
+    :return: compass cardinal direction
+    """
+    if 348.75 > angle < 11.25:
+        return 'North'
