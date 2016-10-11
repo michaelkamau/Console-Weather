e274ce2fd1f28e5e24908c2ecf7ac676b3ee5c4d
Michael Kamau <tokamau@gmail.com>
2016-10-11 12:32:40 +0300

    Update to extract import weather info into a dict
    This info is held in variables, to used in displaying

--- console_weather/weather.py
+++ console_weather/weather.py
@@ -1,5 +1,8 @@
 import json
-import  urllib.request
+import urllib.request
+import datetime
+from console_weather.utilities import degrees_to_direction
+
 
 # TODO: Complete intro string
 intro = 'Welcome to Console Weather'
@@ -20,12 +23,29 @@ print(weather_data)
 
 # Get important parameters
 desc = weather_data['weather'][0]['description'].capitalize() # weather summary
-dt = weather_data['dt'] # date timestamp
+dt = datetime.datetime.fromtimestamp(weather_data['dt']) # date timestamp
 date_obtained = dt.strftime("%A, %d %B %Y %I:%M%p") # date report obtained
 temp = weather_data['main']['temp'] # temperature
 pressure = weather_data['main']['pressure'] # pressure
 humidity = weather_data['main']['humidity'] # humidity
+wind_direction = degrees_to_direction(weather_data['wind']['deg']) # wind direction
+wind_speed = weather_data['wind']['speed'] # wind speed
+sr = datetime.datetime.fromtimestamp(weather_data['sys']['sunrise'])
+sunrise = sr.strftime("%I:%M%p") # sunrise
+st = datetime.datetime.fromtimestamp(weather_data['sys']['sunset'])
+sunset = st.strftime("%I:%M%p") # sunset
 
+# Output weather data obtained
+print("\n\n")
+print('Weather  in ', place.capitalize(), '\n')
+print('Description\t:', desc)
+print('Date obtained\t:', date_obtained)
+print('Temperature\t:', temp)
+print('Pressure\t:', pressure)
+print('Humidity\t:', humidity)
+print('Wind\t:', 'Direction\t:', wind_direction, 'Speed:\t', wind_speed)
+print('Sunrise\t:', sunrise)
+print('Sunset\t:', sunset)
 
 
 
